eea181104ce432bc89c04e01dcdfd096e33e63db
YutaKakiki <yutakakiki@kakinokiyuunoMacBook-Air.local>
2025-02-07 22:23:30 +0900

    husky+lint-stagedの確認

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <p>Hello,world</p>
+        <p>Hello,worlddddd</p>
         <a href="https://vite.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
